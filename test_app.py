d7a2fe6ab5624ffe4858782c6b01710eed87b7e4
Valera1488-s <zvezda.smerti148@gmail.com>
2025-01-27 13:42:14 +0000

    Add CI workflow

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import app
+import unittest
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.app.testing = True
+
+    def test_hello(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.data, b'Hello, World!')
+
+if name == 'main':
+    unittest.main()
