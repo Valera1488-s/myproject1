7b72d90f95d48372a727103d8dc8a680f055d5de
Valera1488-s <zvezda.smerti148@gmail.com>
2025-01-27 14:11:36 +0000

    Fix Flask app CI configuration

--- test_app.py
+++ test_app.py
@@ -11,5 +11,5 @@ class TestApp(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.data, b'Hello, World!')
 
-if name == 'main':
+if __name__ == '__main__':
     unittest.main()
